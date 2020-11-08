e1f43ddf3e20474abfc73a086f9764f3446b234d
saymandal <sayarimandal1218@gmail.com>
2020-11-09 00:21:32 +0530

    Burger Dummy Component
    Setting up Burger component with static data

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends React.Component {
 	render() {
 		return (
 			<Aux>
-				<div>Burger Image</div>
+				<Burger />
 				<div>Burger Controller</div>
 			</Aux>
 		);
