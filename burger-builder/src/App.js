362c4e7aceb5e9f1fbc343c1d62779bdfc7a4942
saymandal <sayarimandal1218@gmail.com>
2020-11-08 14:51:57 +0530

    First Commit
    Setting up the proj, Creating Layout component

---
+++ burger-builder/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+
+class App extends Component {
+	render() {
+		return (
+			<Layout>
+				<h4>Burger Builder !!</h4>
+			</Layout>
+		);
+	}
+}
+
+export default App;
