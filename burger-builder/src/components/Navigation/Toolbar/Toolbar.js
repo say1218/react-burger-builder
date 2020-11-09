38a41e65488cf4f37934c47371b536794b49e3ff
saymandal <sayarimandal1218@gmail.com>
2020-11-10 00:30:27 +0530

    Logo
    Adding logo to toolbar and applying inline style using props

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Logo from "../../Logo/Logo";
 import classes from "./Toolbar.module.css";
 
 const toolbar = (props) => {
 	return (
 		<header className={classes.Toolbar}>
 			<div>MENU</div>
-			<div>LOGO</div>
+			<div className={classes.Logo}>
+				<Logo />
+			</div>
 			<nav>...</nav>
 		</header>
 	);
